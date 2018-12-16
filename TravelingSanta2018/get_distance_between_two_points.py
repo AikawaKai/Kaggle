3ca92947e8bb2ee6d454dfa085082c2862e10df9
Marco Odore <marco.odore@studenti.unimi.it>
2018-12-16 17:12:29 +0100

    updated

--- TravelingSanta2018/get_distance_between_two_points.py
+++ TravelingSanta2018/get_distance_between_two_points.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy
 import time
+import random
+
+
+def dist(x, y):
+    return numpy.sqrt(numpy.sum((x-y)**2))
 
 
 cities = pd.read_csv("data/cities.csv")
@@ -11,15 +16,33 @@ y = list(cities["Y"])
 print(len(x))
 cities_x_y = list(zip(CityId, x, y))
 i = 0
-for id_1, x_1, y_1 in cities_x_y:
-    a = numpy.array((x_1, y_1))
-    i += 1
-    start = time.time()
-    for id_2, x_2, y_2 in cities_x_y:
-        b = numpy.array((x_2, y_2))
-        numpy.linalg.norm(a - b)
-        i += 1
-    end = time.time()
-    print(end-start)
+already_been_there = {}
+all_set = {id: (x, y) for id, x, y in cities_x_y}
 
-print(i)
+first_index = random.randint(0, len(all_set))
+first_city = all_set[first_index]
+all_set.pop(first_index, None)
+print(first_index)
+print(first_city)
+random.shuffle(cities_x_y)
+sets = [first_index]
+print(sets)
+while len(all_set) > 0:
+    b = numpy.array((first_city[0], first_city[1]))
+    shuffled_set = list(all_set.items())
+    random.shuffle(shuffled_set)
+    shuffled_set = shuffled_set[:10]
+    distances = []
+    for key, value in shuffled_set:
+        a = numpy.array((value[0], value[1]))
+        dist_a_b = dist(a, b)
+        distances.append([key, dist_a_b, value[0], value[1]])
+    distances = sorted(distances, key=lambda x: x[1])
+    best = distances[0]
+    sets.append(best[0])
+    all_set.pop(best[0])
+    first_city = (best[2], best[3])
+with open("submission.csv", "w") as f:
+    f.write("Path\n")
+    for line in sets:
+        f.write(str(line)+"\n")
